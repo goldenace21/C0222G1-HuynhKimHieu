9585b6c404f249088c29fe1e3cc837d6de8a2062
HUYNH KIM HIEU <89566149+goldenace21@users.noreply.github.com>
2022-03-07 16:27:03 +0700

    update

---
+++ module_1/ss7_Bien_Kieu_du_lieu_Toan_tu/Thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,12 @@
+let inputwidth;
+let inputheight;
+
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("the area is :" + area)
+
